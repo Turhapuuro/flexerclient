ead2346a68dc84048fac0b8e7f26ff28d17ad9d6
Teemu Salminen <salminen.th@gmail.com>
2018-01-03 15:00:50 +0200

    Add store and example API data fetching

--- src/components/Jaahas.js
+++ src/components/Jaahas.js
@@ -2,24 +2,70 @@ import React, {Component}from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
+import axios from 'axios';
 
 class Jaahas extends Component{
+    state = {
+        users: null,
+        tasks: null
+    }
+
+    fetchUsers = () => {
+        axios.get('/users')
+            .then(response => {
+                this.setState({
+                    users: response.data
+                });
+            });
+    }
+
+    fetchTasks = () => {
+        axios.get('/tasks')
+            .then(response => {
+                this.setState({
+                    tasks: response.data
+                });
+            });
+    }
+
     render(){
         const style = {
             margin: 12,
         };
 
+        let users = null;
+        let tasks = null;
+
+        if (this.state.users !== null) {
+            users = this.state.users.map(user => {
+                return (<li key={user.email}>{user.email}</li>)
+            })
+        }
+
+        if (this.state.tasks !== null) {
+            tasks = this.state.tasks.map(task => {
+                return (<li key={task.name}>{task.name}</li>)
+            })
+        }
+
         return (
             <div>
                 <RaisedButton label="Jaahas" style={style} secondary={true}/>
                 <FontIcon className="material-icons">home</FontIcon>
-                <RaisedButton label="Matias" labelPosition="before" primary={true}
+                <RaisedButton onClick={this.fetchTasks}
+                    label="Matias" labelPosition="before" primary={true}
                     icon={<FontIcon className="material-icons">announcement</FontIcon>}
                     style={style}>
                 </RaisedButton>
-                <IconButton>
+                <IconButton onClick={this.fetchUsers}>
                     <FontIcon className="material-icons">announcement</FontIcon>
                 </IconButton>
+                <ul>
+                    {users}
+                </ul>
+                <ul>
+                    {tasks}
+                </ul>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
+import {ConnectedRouter} from 'react-router-redux';
+import {Provider} from 'react-redux';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const history = createHistory();
+const store = configureStore(history);
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>), document.getElementById('app'));
 registerServiceWorker();
+
+/* ReactDOM.render(<App />, document.getElementById('app'));
+registerServiceWorker();
+ */
