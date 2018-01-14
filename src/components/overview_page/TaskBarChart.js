e502d9bc7dcb3f60d123f98b59ee365d95e93b3c
mathewshore <matias.ranta123@gmail.com>
2018-01-14 18:06:02 +0200

    Add CustomTooltip component to TaskBarChart to exclude tasks which have no hours from rendering

--- src/components/overview_page/TaskBarChart.js
+++ src/components/overview_page/TaskBarChart.js
@@ -14,7 +14,38 @@ const styles = (theme) => ({
         textAlign: 'center',
         cursor: 'default',
     },
+    tooltip: {
+        boxShadow: '#737373 1px 1px 2px',
+        backgroundColor: 'white',
+        padding: 10,
+    },
 });
+
+const CustomTooltip = (props) => {
+    if (!props.active) {
+        return null;
+    }
+
+    const { classes, payload, projectNames } = props;
+    const tooltipData = payload[0].payload;
+    const total = tooltipData.total;
+
+    return (
+        <div className={classes.tooltip}>
+            <div>Total: {total}h</div>
+            {projectNames.map((projectName) => {
+                if (tooltipData[projectName]) {
+                    return (
+                        <div key={`${projectName}-tooltip`}>
+                            {projectName}: {tooltipData[projectName]}h
+                        </div>
+                    );
+                }
+                return null;
+            })}
+        </div>
+    );
+};
 
 class TaskBarChart extends Component {
     getBarFillColor(projectName) {
@@ -53,6 +84,8 @@ class TaskBarChart extends Component {
                             <Label value='Hours' angle={-90} position='insideLeft' />
                         </YAxis>
                         <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip content={
+                            <CustomTooltip classes={classes} projectNames={projectNames} />}/>
                         <Tooltip />
                         {projectNames.map((projectName, i) => (
                             <Bar key={i} dataKey={projectName} stackId='a' fill={this.getBarFillColor(projectName)}>
